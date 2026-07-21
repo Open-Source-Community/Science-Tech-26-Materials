3d55f9d50bc4a2e42c5382ebfcaa79ab22be3599
mariam1617 <145856641+mariam1617@users.noreply.github.com>
2026-07-21 10:44:32 +0300

    upload handson

---
+++ DotNet/Session 4 (LINQ)/HandsOn/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace Session4
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public decimal TotalAmount { get; set; }
+        public bool IsDelivered { get; set; }
+        public List<string> Items { get; set; }
+    }
+
+}
+    
---
+++ DotNet/Session 4 (LINQ)/HandsOn/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Linq;
+namespace Session4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var orders = new List<Order>
+                {
+                    new Order { Id = 1, CustomerName = "Mariam", TotalAmount = 150.0m, IsDelivered = true,  Items = new List<string>{ "Laptop", "Mouse" } },
+                    new Order { Id = 2, CustomerName = "Poula",  TotalAmount = 45.0m,  IsDelivered = false, Items = new List<string>{ "Book" } },
+                    new Order { Id = 3, CustomerName = "Marwa",   TotalAmount = 210.0m, IsDelivered = true,  Items = new List<string>{ "Monitor", "Keyboard", "HDMI Cable" } },
+                    new Order { Id = 4, CustomerName = "Ibrahin",   TotalAmount = 80.0m,  IsDelivered = false, Items = new List<string>{ "Headphones" } }
+                };
+
+
+        }
+    }
+}
